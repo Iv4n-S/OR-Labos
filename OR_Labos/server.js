4c39f9c08cd4df0875682ec9802b561a93d3d2dd
Iv4n-S <is52364@fer.hr>
2022-01-10 22:36:53 +0100

    Labos 4

--- OR_Labos/server.js
+++ OR_Labos/server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const server = express();
 var path = require('path');
 
+require('dotenv').config();
+const port = process.env.PORT || 8080;
+
+const { auth, requiresAuth } = require('express-openid-connect');
+
+server.use(
+  auth({
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET
+  })
+);
+
 const indexRouter = require('./Routes/index.routes');
 const datatableRouter = require('./Routes/datatable.routes');
 
@@ -13,4 +28,8 @@ server.use(express.static(path.join(__dirname, 'public')));
 server.use('/', indexRouter);
 server.use('/datatable', datatableRouter);
 
-server.listen(8080);
+server.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+})
+
+server.listen(port);
